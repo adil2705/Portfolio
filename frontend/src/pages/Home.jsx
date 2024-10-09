9bcf3aad8bff315c0c2c8744c34332fa3e4a7ccc
adil2705 <110901760+adil2705@users.noreply.github.com>
2024-10-09 13:49:43 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,63 +1,21 @@
 import React, { useContext } from "react";
+
 import { HomeContext } from "../context";
 import { HomeInfo, Welcome } from "../components";
-
-const SketchfabEmbed = () => {
-  return (
-    <div className="sketchfab-embed-wrapper" style={{ maxWidth: "100%", height: "500px" }}>
-      <iframe
-        title="Computer and laptop"
-        frameBorder="0"
-        allowFullScreen
-        mozallowfullscreen="true"
-        webkitallowfullscreen="true"
-        allow="autoplay; fullscreen; xr-spatial-tracking"
-        src="https://sketchfab.com/models/8757cd8dd5c941248cc1048fd6368290/embed"
-        style={{ width: "100%", height: "100%" }}
-      ></iframe>
-      <p style={{ fontSize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A" }}>
-        <a
-          href="https://sketchfab.com/3d-models/computer-and-laptop-8757cd8dd5c941248cc1048fd6368290?utm_medium=embed&utm_campaign=share-popup&utm_content=8757cd8dd5c941248cc1048fd6368290"
-          target="_blank"
-          rel="nofollow"
-          style={{ fontWeight: "bold", color: "#1CAAD9" }}
-        >
-          Computer and laptop
-        </a>{" "}
-        by{" "}
-        <a
-          href="https://sketchfab.com/sugaa?utm_medium=embed&utm_campaign=share-popup&utm_content=8757cd8dd5c941248cc1048fd6368290"
-          target="_blank"
-          rel="nofollow"
-          style={{ fontWeight: "bold", color: "#1CAAD9" }}
-        >
-          sugaa
-        </a>{" "}
-        on{" "}
-        <a
-          href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=8757cd8dd5c941248cc1048fd6368290"
-          target="_blank"
-          rel="nofollow"
-          style={{ fontWeight: "bold", color: "#1CAAD9" }}
-        >
-          Sketchfab
-        </a>
-      </p>
-    </div>
-  );
-};
+import SkyCanvas from "../models/Sky";
 
 const Home = () => {
-  const { loadingProgress, hasWelcomeShown } = useContext(HomeContext);
+  const { loadingProgress, setLoadingProgress, hasWelcomeShown } =
+    useContext(HomeContext);
 
   return (
-    <section className="bg-gradient-to-r from-slate-900 to-[#1B1212] min-h-screen relative">
-      {!hasWelcomeShown && loadingProgress < 100 ? <Welcome /> : null}
-      <div className="absolute inset-0 z-10">
+    <section className="fixed inset-0 z-0">
+      {!hasWelcomeShown && loadingProgress < 100 && <Welcome />}
+      <div className="relative w-full h-full overflow-y-auto">
         <HomeInfo />
       </div>
-      <div style={{ position: "fixed", width: "100%", height: "100%" }}>
-        <SketchfabEmbed />
+      <div className="fixed inset-0 z-[-1] bg-gradient-to-r from-slate-900 h-full w-full to-[#1B1212]">
+        <SkyCanvas setLoadingProgress={setLoadingProgress} />
       </div>
     </section>
   );
